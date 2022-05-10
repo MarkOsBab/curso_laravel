6c65fdd8ae16c4996e33db6897e02b067d1a98d6
Marcos García <m.g4089188@gmail.com>
2022-05-10 15:53:37 -0300

    Aplicando la autentificación de la api, mediante tokens y SPA

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,11 +22,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('category/all', [CategoryController::class, 'all']);
-Route::resource('category', CategoryController::class)->except(['create', 'edit']);
 Route::get('category/{category}/post', [CategoryController::class, 'posts']);
 Route::get('category/slug/{slug}', [CategoryController::class, 'slug']);
 
 Route::get('post/all', [PostController::class, 'all']);
 Route::get('post/slug/{slug}', [PostController::class, 'slug']);
-Route::resource('post', PostController::class)->except(['create', 'edit']);
 
+// Usuarios
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::resource('post', PostController::class)->except(['create', 'edit']);
+    Route::resource('category', CategoryController::class)->except(['create', 'edit']);
+});
+
+Route::post('user/login', [UserController::class, 'login']);
+Route::post('user/logout', [UserController::class, 'logout']);
